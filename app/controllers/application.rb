823780cff66d3f17366cdfb988c947f6f42f4365
victormaz <victor.mazurenko@gmail.com>
2008-09-04 11:10:55 -0700

    test

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,5 +1,6 @@
 require 'digest/sha1'
 
+# test
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
